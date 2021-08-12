633e0f09f18b1151e1a6e38d1dace86b92371298
hahas507 <aq12361@gmail.com>
2021-08-12 11:09:39 +0900

    커서 반경 제한
    vector3.clampMagnitude 를 사용해 커서 반경을 제한함.
    다만 y축도 clamp되어 *의도치 않은 기능이 생김.
    
    *플레이어와 목표적이 일정 거리 이상 벌어지면 커서(조준점)이 사라짐.

--- Space Shooter v2/Assets/Scripts/CustomCursor.cs
+++ Space Shooter v2/Assets/Scripts/CustomCursor.cs
@@ -6,7 +6,11 @@ public class CustomCursor : MonoBehaviour
 {
     private Vector3 targetPos;
     private float size;
-    [SerializeField] private float reSizeTo;
+    [SerializeField] private float reSizeTo;//마우스 커서 크기
+
+    [SerializeField] private float maxDistance;//마우스 커서와 플레이어간 한계 거리
+
+    [SerializeField] private Transform playerDis;
 
     private void Start()
     {
@@ -15,9 +19,16 @@ public class CustomCursor : MonoBehaviour
 
     private void Update()
     {
+        Vector3 playerPos = playerDis.localPosition;
+
         size = Camera.main.orthographicSize;
         transform.localScale = Vector3.one * size * reSizeTo;
         targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = targetPos;
+
+        Vector3 offset = targetPos - playerPos;
+
+        //transform.position = targetPos;
+
+        transform.position = playerPos + Vector3.ClampMagnitude(offset, maxDistance);
     }
 }

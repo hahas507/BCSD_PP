479c297fdf3c5b90048ca8c816f6f53d95f84629
hahas507 <aq12361@gmail.com>
2021-08-30 21:45:21 +0900

    타이틀 씬 카메라 움직임 추가
    밋밋한 타이틀씬에 카메라 움직임을 추가함.

---
+++ Space Shooter v2/Assets/Scripts/TitleScene/CamNav.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CamNav : MonoBehaviour
+{
+    private NavMeshAgent navMesh;
+    private int currentWaypoint;
+    [SerializeField] private Transform[] waypoints;
+
+    private void Awake()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        navMesh.Warp(waypoints[0].position);
+    }
+
+    private void Update()
+    {
+        Debug.Log(currentWaypoint);
+        if (navMesh.remainingDistance <= navMesh.stoppingDistance)
+        {
+            StartCoroutine(SetDestination());
+        }
+    }
+
+    private IEnumerator SetDestination()
+    {
+        currentWaypoint++;
+        if (currentWaypoint == waypoints.Length)
+        {
+            currentWaypoint = 0;
+        }
+        navMesh.SetDestination(waypoints[currentWaypoint].position);
+        navMesh.isStopped = true;
+        yield return new WaitForSeconds(3);
+        navMesh.isStopped = false;
+    }
+}

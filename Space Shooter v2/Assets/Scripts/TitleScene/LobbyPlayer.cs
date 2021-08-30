3ef84ab821a00880af903fb8749bc3a8a71b031a
hahas507 <aq12361@gmail.com>
2021-08-30 18:44:01 +0900

    로비씬 메뉴 오브젝트 추가
    Raycast를 이용한 메뉴 추가함

--- Space Shooter v2/Assets/Scripts/TitleScene/LobbyPlayer.cs
+++ Space Shooter v2/Assets/Scripts/TitleScene/LobbyPlayer.cs
@@ -6,39 +6,65 @@ using UnityEngine;
 public class LobbyPlayer : MonoBehaviour
 {
     private Animator ani;
+    private Rigidbody rig;
 
     [SerializeField] private float speed;
+    [SerializeField] private float maxVelocity;
+    [SerializeField] private LayerMask layerMask;
     private bool isWalking = false;
+    private Vector3 playerMovement;
 
     private void Awake()
     {
+        rig = GetComponent<Rigidbody>();
         ani = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         TryMove();
+        DetectMenu();
     }
 
     private void TryMove()
     {
         float inputX = Input.GetAxis("Horizontal");
         float inputZ = Input.GetAxis("Vertical");
-        Vector3 playerMovement = new Vector3(inputX, 0f, inputZ);
+        playerMovement = new Vector3(inputX, 0f, inputZ);
         playerMovement.Normalize();
 
-        transform.Translate(playerMovement * speed * Time.deltaTime, Space.World);
-
+        rig.AddForce(playerMovement * speed, ForceMode.VelocityChange);
         if (playerMovement != Vector3.zero)
         {
             isWalking = true;
             transform.forward = playerMovement;
+
             ani.SetBool("IsWalking", isWalking);
+            if (rig.velocity.magnitude > maxVelocity)
+            {
+                rig.velocity = Vector3.ClampMagnitude(rig.velocity, maxVelocity);
+            }
         }
         else if (playerMovement == Vector3.zero)
         {
             isWalking = false;
+            rig.velocity = Vector3.zero;
             ani.SetBool("IsWalking", isWalking);
+        }
+    }
+
+    private void DetectMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rig.position + Vector3.up, transform.forward, out hitInfo, 10, layerMask))
+            {
+                if (hitInfo.collider.tag == "Menu")
+                {
+                    Debug.Log("OPEN MENU");
+                }
+            }
         }
     }
 }

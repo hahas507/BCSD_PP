6e39823c95cf7a853aff222353e550ae8de94d74
hahas507 <aq12361@gmail.com>
2021-09-02 19:19:35 +0900

    Funnel 작동 방식대폭 수정
    더 이상 Funnel이 랜덤하게 각도를 틀어 해당 경로에 있는 투사체를 파괴하지 않음.
    대신 일정 주기마다 주변에 투사체가 있을 경우 해당 투사체를 향해 바라보고, 해당 투사체를 방향으로 선을 그리고 투사체를 파괴함.

--- Space Shooter v2/Assets/Scripts/Enemy/Boss/Stage_02/Funnel.cs
+++ Space Shooter v2/Assets/Scripts/Enemy/Boss/Stage_02/Funnel.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 
 public class Funnel : MonoBehaviour
 {
-    [Range(0, 60)]
-    [SerializeField] private float maximumAngle;
-
     [SerializeField] private float shakeCycle;
 
     [Range(0, 50)]
@@ -20,10 +17,50 @@ public class Funnel : MonoBehaviour
 
     private bool isLerping;
 
+    private RaycastHit hitInfo;
+    [SerializeField] private LayerMask bulletLayer;
+
+    private Vector3 funnelPosition, tarPosition, funnelAngle, targetDirection;
+
+    [Range(0, 100)]
+    [SerializeField] private float viewRadius;
+
+    [Range(0, 360)]
+    [SerializeField] private float viewAngle;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.enabled = false;
+    }
+
     private void Start()
     {
-        StartCoroutine(ShakeHead());
+        StartCoroutine(LookAtBullet());
         StartCoroutine(RandomPositioning());
+    }
+
+    private float GetDegree(Vector3 _from, Vector3 _to)
+    {
+        return Mathf.Atan2(_to.z - _from.z, _to.x - _from.x) * 180 / Mathf.PI;
+    }
+
+    private void LookAt()
+    {
+        float turretX = transform.position.x;
+        float turretZ = transform.position.z;
+
+        float hitInfoX = hitInfo.transform.position.x;
+        float hitInfoZ = hitInfo.transform.position.z;
+
+        funnelPosition = new Vector3(turretX, 0f, turretZ);
+        tarPosition = new Vector3(hitInfoX, 0f, hitInfoZ);
+
+        funnelAngle = new Vector3(0f, GetDegree(funnelPosition, tarPosition), 0f);
+        transform.eulerAngles = -funnelAngle;
     }
 
     private IEnumerator RandomPositioning()
@@ -39,7 +76,6 @@ public class Funnel : MonoBehaviour
                 transform.localPosition = Vector3.Lerp(transform.localPosition, randomPosition, Time.deltaTime * repositionSpeed);
                 if (Vector3.Distance(transform.localPosition, randomPosition) <= 3)
                 {
-                    Debug.Log("true");
                     isLerping = false;
                 }
                 yield return null;
@@ -47,12 +83,51 @@ public class Funnel : MonoBehaviour
         }
     }
 
-    private IEnumerator ShakeHead()
+    private IEnumerator LookAtBullet()
     {
         while (true)
         {
-            transform.localEulerAngles = new Vector3(0f, Mathf.Clamp(UnityEngine.Random.Range(-maximumAngle, maximumAngle), -maximumAngle, maximumAngle), 0f);
+            Collider[] target = Physics.OverlapSphere(transform.position, viewRadius, bulletLayer);
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                Transform targetTransform = target[i].transform;
+
+                if (targetTransform.tag == "PlayerBullet")
+                {
+                    targetDirection = (targetTransform.position - transform.position).normalized;
+                    float lookAngle = Vector3.Angle(transform.forward, targetDirection);
+                    if (lookAngle < viewAngle / 2)
+                    {
+                        if (Physics.Raycast(transform.position, targetDirection, out hitInfo, viewRadius, bulletLayer))
+                        {
+                            if (hitInfo.transform.tag == "PlayerBullet")
+                            {
+                                LookAt();
+                                lineRenderer.enabled = true;
+                                lineRenderer.SetPosition(0, transform.position);
+                                lineRenderer.SetPosition(1, hitInfo.transform.position);
+                                Destroy(hitInfo.transform.gameObject);
+                            }
+                        }
+                    }
+                }
+            }
+
             yield return new WaitForSeconds(shakeCycle);
+            lineRenderer.enabled = false;
+
+            //transform.localEulerAngles = new Vector3(0f, Mathf.Clamp(UnityEngine.Random.Range(-maximumAngle, maximumAngle), -maximumAngle, maximumAngle), 0f);
+            //Debug.DrawRay(transform.position, Vector3.right, Color.red, 1);
+            //if (Physics.Raycast(transform.position, Vector3.right, out hitInfo, detectMaxDistance, bulletLayer))
+            //{
+            //    if (hitInfo.transform.tag == "PlayerBullet")
+            //    {
+            //        Destroy(hitInfo.transform.gameObject);
+            //    }
+            //}
+
+            //yield return new WaitForSeconds(shakeCycle);
         }
     }
 }
